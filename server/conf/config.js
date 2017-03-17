51705b8dfc2e33ebf9ebd09eac4d8a4d0fb0d47e
杜铭 <milodu@foxmail.com>
2017-03-17 11:30:42 +0800

    fix bug

--- server/conf/config.js
+++ server/conf/config.js
@@ -1,2 +1,2 @@
 //origins
-exports.origins = ['http://a.m.com', 'http://admin.runningdreamer.com', 'http://www.runningdreamer.com', 'http://localhost:8000'];
+exports.origins = ['http://a.m.com', 'https://admin.runningdreamer.com', 'https://www.runningdreamer.com', 'http://localhost:8000'];
