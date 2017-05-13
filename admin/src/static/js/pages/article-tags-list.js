acdb73c1476af6fb8890f3a885c2a5e07c0f85c7
milodu <milodu@foxmail.com>
2017-05-14 01:06:34 +0800

    fix bug

--- admin/src/static/js/pages/article-tags-list.js
+++ admin/src/static/js/pages/article-tags-list.js
@@ -38,10 +38,34 @@ $(function() {
 					this.handleOperate(self, action);
 					break;
 				case 'delete':
-					this.deleteTags(self, action);
+					this.deleteTags(self);
 					break;
 			}
 		},
+		deleteTags: function(slef) {
+			var id = self.attr('data-id');
+			return System.request({
+					type: 'post',
+					url: 'manage/delete_article_tag_info',
+					data: {
+						id: id
+					}
+				})
+				.done(function(response) {
+					if (response.res == 0) {
+						$.toast({
+							icon: 'success',
+							text: '操作成功'
+						});
+						nodes.table.bootstrapTable('refresh');
+					} else {
+						$.toast({
+							icon: 'error',
+							text: response.msg
+						});
+					}
+				});
+		}
 		handleOperate: function(self, type) {
 			var id = self.attr('data-id'),
 				isDelete = self.attr('data-delete') == 1 ? 0 : 1;
@@ -98,7 +122,7 @@ $(function() {
 			return [
 				'<a href="/pages/article-tags-edit.html?id=' + row.id + '">编辑</a>',
 				'<a href="javascript:void(0)" data-action="operate" data-id="' + row.id + '" data-delete="' + row.is_delete[0] + '">' + (row.is_delete[0] == 1 ? '显示' : '隐藏') + '</a>',
-				'<a href="javascript:void(0)" data-action="delete" data-id="' + row.id + '删除' < /a>'
+				'<a href="javascript:void(0)" data-action="delete" data-id="' + row.id + '"删除</a>'
 			].join('&nbsp;');
 		},
 		deleteFormatter: function(value, row, index) {
